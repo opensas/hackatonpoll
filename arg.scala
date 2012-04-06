5dcb4a2e982acee361e11adc07db20174068d52c
Diego Ramirez <diego@diego-note.(none)>
2012-04-06 13:47:56 -0300

    add some samples

---
+++ arg.scala
@@ -0,0 +1,15 @@
+
+args.foreach(arg => if(arg=="puto") println(arg) else println("no puto"))
+val array = (1).to(10)
+println(array.length)
+
+array foreach(numero => println(numero))
+for( i <- 5 to 50) {
+	println(i)
+}
+for( i <- (1).to(5)) {
+	println(i)
+}
+
+println("lo ultimno")
+1 to 3 foreach (numero => println("que groso"))
